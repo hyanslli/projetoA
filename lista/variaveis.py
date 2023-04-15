258e0b6b15a0d5800362b190ea4806051043a327
hyanslli <130848379+hyanslli@users.noreply.github.com>
2023-04-15 12:17:33 -0300

    Create variaveis.py

---
+++ lista/variaveis.py
@@ -0,0 +1,12 @@
+#biblioteca de proprietário
+propietario = {}
+veiculo = {}
+
+#variaveis
+nome_pessoas = None
+modelo_veiculo = None
+placa = None
+cpf = 0
+descricao_car = None
+obs_propietario = None
+opcao = None
