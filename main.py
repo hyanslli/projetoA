abd5fcddc28a2dfaf2a368b8f931fdbc722f540a
hyanslli <130848379+hyanslli@users.noreply.github.com>
2023-04-15 12:17:39 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#Projeto Detra - De Willian Viana de Sousa
+from operacao.operacao import *
+from lista.variaveis import opcao
+
+while True:
+    opcao = cabecalho()
+    match opcao:
+        case 0:
+            print('Saindo...')
+            break
+        case 1:
+            adiciona()
+        
