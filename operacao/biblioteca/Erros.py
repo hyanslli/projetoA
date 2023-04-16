a0a5ce8aa0c39ee411d0e7826b77afc748b050b2
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2023-04-16 16:06:11 -0300

    version 1.10
    Criação da função de edição de dados.
    Adição de alguns comentarios. Alguns ajustes.

--- operacao/biblioteca/Erros.py
+++ operacao/biblioteca/Erros.py
@@ -1,2 +1,10 @@
 def error_dado():
     return "[ERROR] - Dado informado não foi registrado!!!"
+
+
+def error_dado_existente():
+    return "[ERROR] - Dado já registrado!!!"
+
+
+def error_opcao():
+    return "[ERROR] - Digite apenas os números apresentados!!!"
